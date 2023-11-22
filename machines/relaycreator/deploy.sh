988b6a451b46235c04044476105c4a64531e9abb
jeremyd <4072+jeremyd@users.noreply.github.com>
2023-11-22 13:09:38 -0800

    adding firstrun to deploy

--- machines/relaycreator/deploy.sh
+++ machines/relaycreator/deploy.sh
@@ -1,14 +1,24 @@
 #!/bin/bash -e
 
-cd /app
-git remote update
-
-if git status -uno |grep "is behind"
-then
+deploy_app() {
     echo "detected upstream changes, deploying"
     git pull
     npx prisma migrate deploy
-    npx primsa generate
+    npx prisma generate
     npm run build
     systemctl restart app
+}
+
+cd /app
+git remote update
+
+if [ ! -f "/firstrun.txt" ]; then
+    deploy_app
+    touch /firstrun.txt
+    exit 0
+fi
+
+if git status -uno |grep "is behind"
+then
+    deploy_app
 fi
