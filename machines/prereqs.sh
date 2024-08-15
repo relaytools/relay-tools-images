7d3c72e22c2ac137c6d644b40c6da4d357a561b7
jeremyd <4072+jeremyd@users.noreply.github.com>
2024-08-14 21:46:30 -0700

    sysctl settings added to prereqs.sh for higher inotify

--- machines/prereqs.sh
+++ machines/prereqs.sh
@@ -4,3 +4,16 @@
 export DEBIAN_FRONTEND=noninteractive
 apt update -y
 apt install -y net-tools systemd-container debootstrap xz-utils micro
+
+if grep "max_user_watches" /etc/sysctl.conf; then
+    echo "max_user_watches already set"
+else
+    echo fs.inotify.max_user_watches=524288 | tee -a /etc/sysctl.conf && sysctl -p
+fi
+
+if grep "max_user_instances" /etc/sysctl.conf; then
+    echo "max_user_instances already set"
+else
+    echo fs.inotify.max_user_instances=8192 | tee -a /etc/sysctl.conf && sysctl -p
+fi
+
