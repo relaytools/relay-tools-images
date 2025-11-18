ff10dae656ade8c524955df14b1cf2297e570b41
jeremyd <4072+jeremyd@users.noreply.github.com>
2025-11-18 12:08:56 -0800

    removing un-necessary keys-certs-manager image, changing configure to use haproxy instead

--- machines/build-and-publish.sh
+++ machines/build-and-publish.sh
@@ -17,10 +17,8 @@ machinectl export-tar strfry export/strfry.tar.gz
 machinectl export-tar haproxy export/haproxy.tar.gz 
 machinectl export-tar mysql export/mysql.tar.gz 
 machinectl export-tar relaycreator export/relaycreator.tar.gz 
-machinectl export-tar keys-certs-manager export/keys-certs-manager.tar.gz 
 
 aws s3 cp export/strfry.tar.gz s3://relay-tools-images/strfry.tar.gz
 aws s3 cp export/haproxy.tar.gz s3://relay-tools-images/haproxy.tar.gz
 aws s3 cp export/mysql.tar.gz s3://relay-tools-images/mysql.tar.gz
 aws s3 cp export/relaycreator.tar.gz s3://relay-tools-images/relaycreator.tar.gz
-aws s3 cp export/keys-certs-manager.tar.gz s3://relay-tools-images/keys-certs-manager.tar.gz
